f477cdc1d3c24f641e812d2392da834f2ce8452a
unknown <944325009@qq.com>
2025-01-31 21:05:07 +0000

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -5,6 +5,15 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number;
+    };
+}
+
 const friend1 = {
     name: "Paul Fleming",
     phone: "087-12345",
